46d325f12c1134b55ff8bbccb5115d9cec6bc4f9
cristinaaajs@gmail.com <cristinaaajs@gmail.com>
2022-11-06 19:07:09 -0300

    aula_75_EstruturaDados

---
+++ src/AgesArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AgesArray {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        int[] age = new int[5];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < age.length; i++) {
+
+            System.out.print("Informe a idade " + i + ":");
+            age[i] = scanner.nextInt();
+        }
+
+        for (int a : age) {
+            System.out.println("Idade: " + a);
+        }
+
+    }
+}
